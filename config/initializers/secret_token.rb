e357e1eec3ab0226a8d9bc896e3dff225dab1516
push2pull <james.roitberg@gmail.com>
2012-02-07 11:56:41 -0200

    junior 07/02/12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unifloor::Application.config.secret_token = '5acabdb8cd69770a578918acd92165a6e4f743662f9171421bade805c03ff9058b06817ffbda0a3a6a0afd6f195383243edaa5e08b8c8522ae1098739854887c'
