43dd04f48488455d667e19912a8caad698e46ebf
James Roitberg <james.roitberg@gmail.com>
2012-05-17 16:26:13 -0300

    created user on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'admin@admin.com', password: 'abc123', password_confirmation: 'abc123', access_type: AccessTypes::ADMIN)
