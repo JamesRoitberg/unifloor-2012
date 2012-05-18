eb5ae6a480cdc4755dbc4321ed4ec6a55c05fca9
James Roitberg <james.roitberg@gmail.com>
2012-05-18 08:57:06 -0300

    migrate customer on database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120518033808) do
+ActiveRecord::Schema.define(:version => 20120518034458) do
 
   create_table "customers", :force => true do |t|
     t.string   "name"
@@ -78,6 +78,12 @@ ActiveRecord::Schema.define(:version => 20120518033808) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
+  create_table "users_categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "users_orders", :force => true do |t|
     t.string   "code"
     t.integer  "costumer_id"
@@ -90,5 +96,14 @@ ActiveRecord::Schema.define(:version => 20120518033808) do
 
   add_index "users_orders", ["costumer_id"], :name => "index_users_orders_on_costumer_id"
   add_index "users_orders", ["user_id"], :name => "index_users_orders_on_user_id"
+
+  create_table "users_products", :force => true do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "users_products", ["category_id"], :name => "index_users_products_on_category_id"
 
 end
