e68af95baf884dc9f7efeb028d9c3ea6e36880f4
Renan Oronfle <renanoronfle@gmail.com>
2012-05-22 13:37:29 -0300

    some things

--- app/controllers/users/stocks_controller.rb
+++ app/controllers/users/stocks_controller.rb
@@ -41,7 +41,7 @@ class Users::StocksController < ApplicationController
 
     respond_to do |format|
       if @stock.save
-        format.html { redirect_to [:users, @stock], notice: 'Stock was successfully created.' }
+        format.html { redirect_to users_stocks_path, notice: 'Stock was successfully created.' }
       else
         format.html { render action: "new" }
         format.json { render json: @stock.errors, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class Users::StocksController < ApplicationController
 
     respond_to do |format|
       if @stock.update_attributes(params[:stock])
-        format.html { redirect_to [:users, @stock], notice: 'Stock was successfully updated.' }
+        format.html { redirect_to users_stocks_path, notice: 'Stock was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -71,8 +71,7 @@ class Users::StocksController < ApplicationController
     @stock.destroy
 
     respond_to do |format|
-      format.html { redirect_to stocks_url }
-      format.json { head :no_content }
+      format.html { redirect_to users_stocks_path }
     end
   end
 end
